484bc514193aae0a8827dbcda436efb83c31e284
Nikhil <purinikhil42@gmail.com>
2026-07-30 18:29:30 +0530

    another pandaas push

---
+++ pandas/pandasDataframe.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+
+# ======Pandas Dataframe==========
+
+# using lists 
+student_data = [
+    [100,56,12],
+    [94,58,14],
+    [88,64,20]
+]
+print(pd.DataFrame(student_data,columns=['iq','marks','package']))
+
+# using dicts
+student_data1 = {
+    'iq':[100,94,88],
+    'marks':[56,58,64],
+    'package':[12,14,20]
+}
+student = pd.DataFrame(student_data1)
+print(pd.DataFrame(student_data1))
+
+# using read_csv
+
+movies = pd.read_csv(r"C:\Users\NIKHIL\OneDrive\Documents\Desktop\BasicToAd.py\csv_files\movies.csv")
+print(movies)
+
+ipl = pd.read_csv(r"C:\Users\NIKHIL\OneDrive\Documents\Desktop\BasicToAd.py\csv_files\ipl-matches.csv")
+print(ipl)
+
+# ========Dataframe Attributes and Methods==========
+
+# shape
+print(ipl.shape)
+print(movies.shape)
+
+# dtypes
+print(ipl.dtypes)
+print(movies.dtypes)
+
+# index
+print(ipl.index)
+print(movies.index)
+
+# columns
+print(ipl.columns)
+
+# values
+print(ipl.values)
+
+# head and tail
+print(movies.head(2))
+print(ipl.tail(7))
+
+# sample
+print(ipl.sample())
+
+# info
+print(movies.info())
+
+# describe
+print(movies.describe())
+
+# isnull
+print(movies.isnull().sum())
+
+# duplicated
+print(movies.duplicated().sum())
+
+# rename
+student.rename(columns={'package':'lpa'},inplace=True)
+print(student)
--- pandas/seriesUsingCSV.py
+++ pandas/seriesUsingCSV.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 
-# Series Using read_csv
+
+# 1)/////Series Using read_csv
+
 
 ##### with 1 column#######
 
@@ -20,7 +22,9 @@ print(vk)
 movies = pd.read_csv(r"C:\Users\NIKHIL\OneDrive\Documents\Desktop\BasicToAd.py\csv_files\bollywood.csv",index_col="movie").squeeze("columns")
 print(movies)
 
-# Series Methods
+
+# 2)//////Series Methods
+
 
 ####head and tail######
 print(movies.head())
@@ -32,3 +36,64 @@ print(subs.tail(10))
 
 print(vk.sample())
 print(movies.sample(10))
+
+#######value-counts->movie##########
+print(movies.value_counts())
+
+########sort values##############
+print(vk.sort_values(ascending=False).head(1).values[0])
+
+#########inplace##########
+# vk.sort_values(inplace=True)
+# print(vk) #(inplace changes the original vk series not in the temp)
+
+########sort_index########
+print(movies.sort_index(ascending=False))
+
+
+# 3)//////Series Math methods
+
+#########count##############
+print(vk.count())
+
+#########sum,product##########
+print(subs.sum())
+print(subs.product())
+
+########mean->median->mode->std->var########
+print(subs.mean())
+print(vk.median())
+print(movies.mode())
+print(subs.std())
+print(vk.var())
+
+############min->max########
+print(subs.min())
+print(subs.max())
+
+######describe########
+print(vk.describe())
+
+
+# 4)//////Series indexing
+x = pd.Series([16,45,21,3,2,54,12,78,99])
+print(x[0])
+
+print(movies['Uri: The Surgical Strike'])
+print(vk[1])
+
+
+# 5)//////////Series slicing
+print(vk[5:16])
+print(vk[-5:])
+print(movies[::2])
+
+
+# 6)///////Fancy indexing
+print(vk[[1,3,4,5]])
+
+
+# 7)/////////Series editing
+x[1] = 100
+print(x)
+
